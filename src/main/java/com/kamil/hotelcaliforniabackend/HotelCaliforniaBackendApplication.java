b17cfeb615c32358bcc5560ecbf06171190e3dc1
Kamil <kamil.raczkowski@onet.pl>
2024-12-04 11:09:31 +0100

    Initial commit

---
+++ src/main/java/com/kamil/hotelcaliforniabackend/HotelCaliforniaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kamil.hotelcaliforniabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelCaliforniaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelCaliforniaBackendApplication.class, args);
+    }
+
+}
